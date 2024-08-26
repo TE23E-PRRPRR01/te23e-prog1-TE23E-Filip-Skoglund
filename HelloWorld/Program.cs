4e70da360f5d451db9859aa51e7c29b9b4cd634e
Filip Skoglund School <filip.skoglund@elev.ntig.se>
2024-08-26 14:24:50 +0200

    Project: HelloWorld

---
+++ HelloWorld/Program.cs
@@ -0,0 +1,26 @@
+﻿Console.Clear();
+
+
+Console.WriteLine("hello world");
+Console.WriteLine("the one and only skogis has spawned");
+
+Console.Write("Vad heter du? \n");
+string namn = Console.ReadLine();
+Console.WriteLine("Hej " + namn);
+
+Console.ForegroundColor = ConsoleColor.Red;
+Console.BackgroundColor = ConsoleColor.White;
+Console.Write("Vad brukar du göra du på fritiden? \n");
+string fritid = Console.ReadLine();
+Console.ResetColor();
+
+Console.ForegroundColor = ConsoleColor.Blue;
+Console.BackgroundColor = ConsoleColor.Yellow;
+Console.WriteLine("Nämen, vad roligt! Jag tycker också att " + fritid + " är kul");
+Console.ResetColor();
+
+Console.ForegroundColor = ConsoleColor.Green;
+Console.BackgroundColor = ConsoleColor.Cyan;
+Console.Write("Såg du på os i sommar? Vilken sport tyckte du var bäst? \n");
+string os = Console.ReadLine();
+Console.ResetColor();
