fea812d523d1f4521728164d9cf33437f51e8c2e
Filip Skoglund School <filip.skoglund@elev.ntig.se>
2024-08-26 14:30:37 +0200

    Added Project: Registrering

---
+++ Kapitel-1/Registrering/Program.cs
@@ -0,0 +1,11 @@
+﻿// Program för att registrera för-/efternamn
+Console.Clear();
+Console.WriteLine("Program för registering av för och efternamn...");
+
+// User Input
+Console.Write("Ange förnamn: ");
+string förnamn = Console.ReadLine();
+Console.Write("Ange efternamn: ");
+string efternamn = Console.ReadLine();
+
+Console.WriteLine($"Hej {förnamn} {efternamn}");
