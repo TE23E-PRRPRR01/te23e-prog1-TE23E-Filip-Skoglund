6e88e63325bf7e6bc53ed3f015678c28e0cb2b1e
Filip Skoglund <filip.skoglund@elev.ntig.se>
2024-08-26 14:45:37 +0200

    Projekt: Registrering (added if statements)

--- Kapitel-1/Registrering/Program.cs
+++ Kapitel-1/Registrering/Program.cs
@@ -7,5 +7,18 @@ Console.Write("Ange förnamn: ");
 string förnamn = Console.ReadLine();
 Console.Write("Ange efternamn: ");
 string efternamn = Console.ReadLine();
-
 Console.WriteLine($"Hej {förnamn} {efternamn}");
+
+System.Threading.Thread.Sleep(2000);
+Console.Clear();
+
+// Är det Melvin som loggar in?
+if (förnamn == "Melvin" && efternamn == "Boholm")
+{
+    Console.WriteLine($"Välkommen tillbaka, {förnamn}!");
+}
+else
+    Console.WriteLine($"Behörighet Saknas! Programet termineras...");
+System.Threading.Thread.Sleep(3000);
+Console.Clear();
+
