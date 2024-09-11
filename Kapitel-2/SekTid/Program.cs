73fc54ed49c31d4088a3e7bb9089465452e88f51
SkogisAB <filip.skoglund07@outlook.com>
2024-09-11 14:55:35 +0200

    Project: SekTid

---
+++ Kapitel-2/SekTid/Program.cs
@@ -0,0 +1,19 @@
+﻿// Program för att konvertera sekunder till timmar:minuter:sekunder
+Console.Clear();
+Console.WriteLine("Detta program konverterar sekunder till formatet timmar:minuter:sekunder");
+Thread.Sleep(500);
+Console.WriteLine("Tryck på Enter för att fortsätta...");
+while (Console.ReadKey(true).Key != ConsoleKey.Enter)
+{
+    // Väntar på ENTER...
+}
+
+Console.Clear();
+Console.Write("Ange antal sekunder: ");
+int totalSeconds = int.Parse(Console.ReadLine());
+
+int hours = totalSeconds / 3600;
+int minutes = totalSeconds % 3600 / 60;
+int seconds = totalSeconds % 60;
+
+Console.WriteLine($"{hours:00}:{minutes:00}:{seconds:00}");
