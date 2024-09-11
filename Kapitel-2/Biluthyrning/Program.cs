13616ba354904442b9977eb65ed777e33caede92
SkogisAB <filip.skoglund07@outlook.com>
2024-09-11 14:44:49 +0200

    Project: Biluthyrning

---
+++ Kapitel-2/Biluthyrning/Program.cs
@@ -0,0 +1,16 @@
+﻿// Program för att beräkna kostnaden för att hyra bil hos en biluthyrningsfirma.
+Console.Clear();
+int milKostnad = 5;
+int dagKostnad = 100;
+
+Console.WriteLine("Välkommen till biluthyrningsfirman!");
+Thread.Sleep(1500);
+Console.Clear();
+Console.Write("Ange antal mil du ska köra: ");
+int antalMil = int.Parse(Console.ReadLine());
+Console.Write("Ange antal dagar du ska hyra bilen: ");
+int antalDagar = int.Parse(Console.ReadLine());
+
+int totalKostnad = milKostnad * antalMil + dagKostnad * antalDagar + 500;
+
+Console.WriteLine($"Total kostnad: {totalKostnad}kr");
