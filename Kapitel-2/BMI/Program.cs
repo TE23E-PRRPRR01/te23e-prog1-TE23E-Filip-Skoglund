f56176e6034453403d236ff71928b7be6f99bb69
SkogisAB <filip.skoglund07@outlook.com>
2024-09-11 14:34:12 +0200

    Project: BMI Calculator

---
+++ Kapitel-2/BMI/Program.cs
@@ -0,0 +1,15 @@
+﻿// Program för att räkna BMI
+Console.Clear();
+
+Console.WriteLine("Välkommen till BMI-kalkylatorn!");
+Thread.Sleep(1000);
+
+Console.WriteLine("Ange din längd i meter: ");
+double längd = double.Parse(Console.ReadLine());
+
+Console.WriteLine("Ange din vikt i kilogram: ");
+double vikt = double.Parse(Console.ReadLine());
+
+double bmi = vikt / (längd * längd);
+
+Console.WriteLine($"Ditt BMI är: {Math.Round(bmi, 2)}");
