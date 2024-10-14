396ae8d7689996c5348c961ca871eb2586c42e2c
SkogisAB <filip.skoglund07@outlook.com>
2024-10-14 14:49:00 +0200

    Övat på listor (och gjorde en funktion som plus)

---
+++ Kapitel-5/Listor/Program.cs
@@ -0,0 +1,56 @@
+﻿// Använda lsitor för att skapa/hantera samlingar
+Console.Clear();
+
+
+/* -------------------- Genomgång -------------------- */
+// Lista på trevliga frukter, tex "äpple", "appelsin", "annanas"
+List<string> frukter = ["äpple", "apelsin", "annanas"];
+
+// Ändra i listan
+frukter[0] = "banan";
+
+// Gå igenom listan = loopa igenom = från första till sista
+foreach (var frukt in frukter)
+{
+    Console.WriteLine(frukt);
+}
+Console.WriteLine("\n");
+
+
+/* -------------------- Uppgift: Skriva ut med loopar -------------------- */
+// Skapa en lista på valfria saker (4st)
+List<string> saker = ["kyckling", "nötkött", "fläsk", "fisk"];
+
+// Skriv ut varje sak på samma rad (foreach och for loop)
+foreach (var sak in saker)
+{
+    Console.Write($"{sak} ");
+}
+
+Console.WriteLine();
+
+for (int i = 0; i < saker.Count; i++)
+{
+    Console.Write($"{saker[i]} ");
+}
+Thread.Sleep(1500);
+Console.Clear();
+
+/* -------------------- Uppgit: Skapa register -------------------- */
+Console.WriteLine("Uppgift: Skapa namnregister");
+
+// En tom lista
+List<string> namnlista = [];
+
+// while-loop 
+while (true)
+{
+    Console.Write("\nAnge ett namn: ");
+    namnlista.Add(Console.ReadLine());
+
+    // Skriv ut hela listan
+    foreach (var namn in namnlista)
+    {
+        Console.Write($"{namn} ");
+    }
+}
---
+++ Kapitel-5/Namnregister/Program.cs
@@ -0,0 +1,56 @@
+﻿// Ett litet namnregister
+Console.Clear();
+Console.ForegroundColor = ConsoleColor.White;
+Console.WriteLine("Ett litet namnregister");
+
+// Skapa en lista
+List<string> namnlista = [];
+
+// Programloopen
+while (true)
+{
+    // Visa en meny 
+    Console.WriteLine("");
+    Console.Write("""
+1. Registrera namn
+2. Skriva ut registret
+3. Avluta programmet
+Ange ditt val: 
+""");
+    string val = Console.ReadLine();
+
+    if (val == "1")
+    {
+        Console.Write("\nAnge ett namn: ");
+        namnlista.Add(Console.ReadLine());
+        WriteLineColored("+ registrerade namnet", ConsoleColor.Green);
+    }
+    else if (val == "2")
+    {
+        Console.WriteLine(" ");
+        foreach (var namn in namnlista)
+        {
+            Console.Write($"{namn}, ");
+        }
+    }
+    else if (val == "3")
+    {
+        WriteLineColored("Avslutar Programmet", ConsoleColor.Red);
+        break;
+    }
+    else
+    {
+        WriteLineColored("Ogiltigt val, försök igen", ConsoleColor.DarkYellow);
+        Thread.Sleep(500);
+        Console.Clear();
+    }
+
+}
+
+
+static void WriteLineColored(string message, ConsoleColor color)
+{
+    Console.ForegroundColor = color;
+    Console.WriteLine(message);
+    Console.ForegroundColor = ConsoleColor.White;
+}
