e8044aff564fc0b8b13d1b275e892a970937c4e0
SkogisAB <filip.skoglund07@outlook.com>
2025-01-08 14:43:07 +0100

    Intro på metoder

---
+++ Kapitel-6/MetoderIntro/Program.cs
@@ -0,0 +1,51 @@
+﻿/* **************************************************
+* ********************  M A I N  ********************
+*************************************************** */
+
+// Intro till metoder
+Console.Clear();
+Console.WriteLine("Hej metoder!");
+
+// Anropa metoden
+SägHej();
+
+// Anropa metoden meny
+VisaMeny();
+
+// Anropa metoden SägHejTill
+SägHejTill("Micke");
+
+
+
+/* **************************************************
+* *****************  M E T O D E R  *****************
+*************************************************** */
+/// <summary>
+/// Metod som skirver ut en hälsning
+/// </summary>
+static void SägHej()
+{
+Console.WriteLine("Hej på dig!");
+}
+
+/// <summary>
+/// Metod som skriver ut en meny
+/// </summary>
+static void VisaMeny()
+{
+    Console.WriteLine("""
+    1. Lägg till uppgift
+    2. Ta bort uppgift
+    3. Visa uppgifter
+    4. Avsluta
+    """);
+}
+
+/// <summary>
+/// Metod som hälsar på en person med valfritt namn
+/// </summary>
+/// <param name="namn"></param>
+static void SägHejTill(string namn)
+{
+    Console.WriteLine($"Hej {namn}!");
+}
