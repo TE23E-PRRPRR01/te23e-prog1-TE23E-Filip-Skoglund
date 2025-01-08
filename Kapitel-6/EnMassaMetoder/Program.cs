254363dc6e4e1ba924b3c6a64ba96c82f97c5900
SkogisAB <filip.skoglund07@outlook.com>
2025-01-08 15:03:00 +0100

    En massa metoder

---
+++ Kapitel-6/EnMassaMetoder/Program.cs
@@ -0,0 +1,37 @@
+﻿
+/* **************************************************
+*                      M A I N   
+*************************************************** */
+
+Console.Clear();
+Console.WriteLine("Hello, World!");
+
+SägHejTill("Elias");
+SägHejTill("Micke");
+
+TalIKvadrat(4);
+TalIKvadrat(8);
+
+
+/* **************************************************
+*                    M E T H O D S  
+*************************************************** */
+
+/// <summary>
+/// Metod som hälsar på en person med valfritt namn
+/// </summary>
+/// <param name="namn"></param>s
+static void SägHejTill(string namn)
+{
+    Console.WriteLine($"Hej {namn}!");
+}
+
+/// <summary>
+/// Metod som tar ett nummer och skriver ut det i kvadrat
+/// </summary>
+/// <param name="tal"></param>
+static void TalIKvadrat(double tal)
+{
+    Console.WriteLine($"Talet {tal} i kvadrat är {tal*tal}");
+}
+
